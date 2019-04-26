f210fa8de83c4cfbae242910476c61cbe1570177
Joe <joechudz@gmail.com>
2019-04-25 22:11:29 -0500

    added some comments to clarify things

--- GraphSearchDjikstra.js
+++ GraphSearchDjikstra.js
@@ -82,11 +82,11 @@ GraphSearchDjikstra.prototype.canIMakeIt = function(graph, pathOut, pathBack, da
 	var totalCostOut = 0;
 	var totalCostBack = 0;
 
-	var daysOut = daysOfTrip/2;
-	var hoursDrivingOut = daysOut*hoursDriving;
+	var daysOut = daysOfTrip/2; // splitting the time in half (should take at most half the time for the whole trip to get there)
+	var hoursDrivingOut = daysOut*hoursDriving; // total amount of driving hours for the user inputted time to get there
 
-	var daysBack = daysOfTrip/2;
-	var hoursDrivingBack = daysBack*hoursDriving;
+	var daysBack = daysOfTrip/2; // splitting the time in half (should take at most half the time for the whole trip to get there)
+	var hoursDrivingBack = daysBack*hoursDriving; // total amount of driving hours for the user inputted time to get there
 
 	for (var i = 1; i < pathOut.length; i++){
 		totalCostOut = totalCostOut + (graph.getEdge(pathOut[i-1],pathOut[i]).getEdgeCost());
@@ -95,6 +95,7 @@ GraphSearchDjikstra.prototype.canIMakeIt = function(graph, pathOut, pathBack, da
 	var hoursNeededDrivingOut = totalCostOut/speed;
 	// testing if can make it there in time
 	if (hoursDrivingOut < hoursNeededDrivingOut) {
+		// if cant make it to destination with user inputted time constraints, figure out the amount of days needed for entire trip based on hours per day driving
 		var daysNeeded = (2*totalCostOut)/(speed*hoursDriving);
 		return ["Out", Math.round(hoursNeededDrivingOut), daysOfTrip, Math.ceil(daysNeeded), hoursDriving];
 	}
@@ -105,10 +106,12 @@ GraphSearchDjikstra.prototype.canIMakeIt = function(graph, pathOut, pathBack, da
 		}
 		var hoursNeededDrivingBack = totalCostBack/speed;
 		if (hoursDrivingBack < hoursNeededDrivingBack) {
+			// if cant make it back to starting point with user inputted time constraints, figure out the amount of days needed for entire trip based on hours per day driving
 			var daysNeeded = (2*totalCostBack)/(speed*hoursDriving);
 			return ["Back", Math.round(hoursNeededDrivingBack), daysOfTrip, Math.ceil(daysNeeded), hoursDriving];
 		}
 		else {
+			// everything is good and the user inputted time constraints check out. they can go on the trip and make it back to the starting point within the time constraints
 			return ["Good", Math.round(hoursDrivingOut)];
 		}
 	}
